e034be28254ce58dbbd3768076f50fa678d38d30
Caleb Madrigal <caleb.madrigal@gmail.com>
2025-09-24 23:21:32 -0400

    Insert Math functionality

--- do_build.sh
+++ do_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm install
-npx parcel build src/index.html --no-source-maps --dist-dir dist
+npx parcel build src/index.html --no-source-maps --dist-dir dist # --no-optimize # blows up html from 1.25 to 4.1
 cp images/* dist
