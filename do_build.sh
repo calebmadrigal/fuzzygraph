d952234cf49c5b262e0cde3287ed9b9f642f0a9a
Caleb Madrigal <caleb.madrigal@gmail.com>
2025-09-27 11:31:05 -0400

    Examples

--- do_build.sh
+++ do_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 npm install
 npx parcel build src/index.html --no-source-maps --dist-dir dist # --no-optimize # blows up html from 1.25 to 4.1
-cp images/* dist
+cp src/examples.html dist
+cp -r images dist
