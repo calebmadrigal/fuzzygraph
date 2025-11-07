33ba34cb52f292e024fa07c5601b1a56af92b19c
Caleb Madrigal <caleb.madrigal@gmail.com>
2025-11-07 13:23:35 -0500

    Updated favicon - v1.0.41

--- do_build.sh
+++ do_build.sh
@@ -3,3 +3,11 @@ npm install
 npx parcel build src/index.html --no-source-maps --dist-dir dist # --no-optimize # blows up html from 1.25 to 4.1
 cp src/examples.html dist
 cp -r images dist
+
+# favicon
+cp images/android-chrome-192x192.png dist
+cp images/android-chrome-512x512.png dist
+cp images/apple-touch-icon.png dist
+cp images/favicon-16x16.png dist
+cp images/favicon-32x32.png dist
+cp images/favicon.ico dist
