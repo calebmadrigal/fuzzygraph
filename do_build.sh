86c4988026f2a7176b43f90bbacbb5d0357308e0
Caleb Madrigal <caleb.madrigal@gmail.com>
2025-11-21 17:32:55 -0500

    Made link generation versioned, fixed more stuff

--- do_build.sh
+++ do_build.sh
@@ -13,6 +13,9 @@ cp -r images dist
 # Copy previous versions to dist
 curl -L -o dist/v1.html https://github.com/calebmadrigal/fuzzygraph/releases/download/v1.0.43/FuzzyGraph_v1.0.43.html
 
+# Copy current version to a versioned link
+cp dist/index.html dist/v2.html  # NOTE: Make sure to change this from v2.html to v3.html (and add v2.html curl copy as above)
+
 # Copy favicon
 cp images/android-chrome-192x192.png dist
 cp images/android-chrome-512x512.png dist
