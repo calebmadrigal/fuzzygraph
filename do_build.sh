0f63594785bcc21de5ce212a595ac29cd4bf40df
Caleb Madrigal <caleb.madrigal@gmail.com>
2025-11-21 13:52:00 -0500

    partial changes for v2.0.0

--- do_build.sh
+++ do_build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
+# Bulid self-contained html file
 npm install
 npx parcel build src/index.html --no-source-maps --dist-dir dist # --no-optimize # blows up html from 1.25 to 4.1
+
+# Copy examples
 cp src/examples.html dist
+
+# Copy images
 cp -r images dist
 
-# favicon
+# Copy previous versions to dist
+curl -L -o dist/v1.html https://github.com/calebmadrigal/fuzzygraph/releases/download/v1.0.43/FuzzyGraph_v1.0.43.html
+
+# Copy favicon
 cp images/android-chrome-192x192.png dist
 cp images/android-chrome-512x512.png dist
 cp images/apple-touch-icon.png dist
